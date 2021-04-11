523c4445b05e30fdd964b014b926f214f95ae40f
maslamphd <81878709+maslamphd@users.noreply.github.com>
2021-04-11 20:30:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request
+import socket
+from datetime import datetime
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+today = datetime.today()
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -6,7 +12,7 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template('form.html')
+    return render_template('index.html')
 
 
 @app.route('/result', methods=['POST'])
@@ -25,7 +31,14 @@ def result():
     else:
         result = 'INVALID CHOICE'
     entry = result
+    
+    #writing to the logFile
+    f = open("logFile.txt", "a")
+    f.write(repr(var_1) + " " + repr(operation) + " " + repr(var_2) + " = " + repr(result) + " Host:" + repr(ip_address) + " Date-Time:" + repr(today.strftime("%d/%m/%Y %H:%M:%S")) + "\n")
+    f.close()
+    
     return render_template('result.html', entry=entry)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
